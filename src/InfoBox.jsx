df979e22b8c2e9da894086ca7d8b49f7a29f12d4
dibyo20 <dibyobanerjee005@gmail.com>
2025-09-09 19:37:45 +0530

    Initial commit of the project

---
+++ src/InfoBox.jsx
@@ -0,0 +1,44 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import "./InfoBox.css";
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import SunnyIcon from '@mui/icons-material/Sunny';
+
+
+export default function InfoBox({ info }) {
+    const INIT_URL = "https://images.unsplash.com/photo-1592210454359-9043f067919b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+    const HOT_URL = "https://images.unsplash.com/photo-1447601932606-2b63e2e64331?q=80&w=1958&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const COLD_URL = "https://images.unsplash.com/photo-1517362302400-873b4e30f5c0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const RAIN_URL = "https://images.unsplash.com/photo-1534274988757-a28bf1a57c17?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+    return (
+        <div className="InfoBox">
+            <h1>WeatherInfo-{info.weather}</h1>
+            <div className='cardContainer'>
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image={info.humidity > 80 ? RAIN_URL : (info.temp > 15) ? HOT_URL : COLD_URL}
+                        title="weather image"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {info.city}&nbsp;{info.humidity > 80 ? <ThunderstormIcon /> : (info.temp > 15) ? <SunnyIcon /> : <AcUnitIcon />}
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary' }} component={"span"}>
+                            <p>Temperature = {info.temp}&deg;C</p>
+                            <p>Humidity = {info.humidity}</p>
+                            <p>Max Temp = {info.tempMax}&deg;C</p>
+                            <p>Min Temp = {info.tempMin}&deg;C</p>
+                            <p>The weather can be described as <b><i>{info.weather}</i></b> and feels like {info.feelsLike}&deg;C</p>
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
+    )
+}
